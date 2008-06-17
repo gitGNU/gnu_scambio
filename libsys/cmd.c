3d78a1f6566d2e02e61ace0fb32bbb06823c9647
Cedric Cellier <rixed@deb.happyleptic.org>
2008-06-17 21:07:33 +0200

    cf TODO librixed

--- libsys/cmd.c
+++ libsys/cmd.c
@@ -1,1 +1,75 @@
 #include "cmd.h"
+
+/*
+ * Data Definitions
+ */
+
+struct cmd {
+	LIST_ENTRY(cmd_keyword) entry;
+	char const *keyword;
+	unsigned nb_arg_min, nb_arg_max;
+	cmd_callback *cb;
+	unsigned nb_types;	// after which STRING is assumed
+	enum cmd_type { CMD_STRING, CMD_INTEGER, CMD_EOA } types[CMD_MAX_ARGS];
+};
+
+static LIST_HEAD(cmds, cmd) cmds;
+static int textfd;
+
+/*
+ * Command (un)registration
+ */
+
+static void cmd_ctor(struct cmd *cmd, char const *keyword, unsigned nb_arg_min, unsigned nb_arg_max, cmd_callback *cb, va_list ap)
+{
+	cmd->keyword = keyword;
+	cmd->nb_arg_min = nb_arg_min;
+	cmd->nb_arg_max = nb_arg_max;
+	cmd->cb = cb;
+	cmd->nb_types = 0;
+	enum cmd_type type;
+	while (CMD_EOA != (type = va_arg(ap, enum cmd_type))) {
+		if (cmd->nb_types >= sizeof_array(cmd->types)) THROW(BAD_PARAMETER);
+		cmd->types[ cmd->nb_types++ ] = type;
+	}
+	LIST_INSERT_HEAD(&cmds, cmd, entry);
+}
+
+static void cmd_dtor(struct cmd *cmd)
+{
+	LIST_REMOVE(cmd, entry);
+}
+
+static struct cmd *cmd_new(char const *keyword, unsigned nb_arg_min, unsigned nb_arg_max, cmd_callback *cb, va_list ap)
+{
+	struct cmd *cmd = malloc(sizeof(*cmd));
+	if (! cmd) THROW(OOM);
+	unwindb();
+	atunwind(free, cmd);
+	cmd_ctor(cmd, keyword, nb_arg_min, nb_arg_max, cb, ap);
+
+}
+
+int cmd_register_keyword(char const *keyword, unsigned nb_arg_min, unsigned nb_arg_max, cmd_callback *cb, ...);
+void cmd_unregister_keyword(char const *keyword);
+
+
+/*
+ * (De)Initialization
+ */
+
+void cmd_begin(int textfd_)
+{
+	textfd = textfd_;
+	LIST_INIT(&cmds);
+}
+
+void cmd_end(void)
+{
+	struct cmd *cmd;
+	while (NULL != (cmd = LIST_FIRST(&cmds))) {
+		cmd_del(cmd);
+	}
+}
+
+
--- libsys/cmd.h
+++ libsys/cmd.h
@@ -16,22 +16,13 @@ union cmd_arg {
 	long long integer;
 };
 
-typedef void cmd_callback(char const *token, unsigned nb_args, union cmd_arg *args);
+typedef void cmd_callback(char const *keyword, unsigned nb_args, union cmd_arg *args);
 
 #define CMD_MAX_ARGS 8
 
-struct cmd_keyword {
-	LIST_ENTRY(cmd_keyword) entry;
-	char const *token;
-	unsigned nb_arg_min, nb_arg_max;
-	unsigned nb_types;	// after which STRING is assumed
-	enum cmd_type { CMD_STRING, CMD_INTEGER, CMD_EOA } types[CMD_MAX_ARGS];
-	cmd_callback *cb;
-};
-
-int cmd_begin(int textfd);
-int cmd_register_keyword(char const *token, unsigned nb_arg_min, unsigned nb_arg_max, cmd_callback *cb, ...);
-void cmd_unregister_keyword(char const *token);
+void cmd_begin(int textfd);
+int cmd_register_keyword(char const *keyword, unsigned nb_arg_min, unsigned nb_arg_max, cmd_callback *cb, ...);
+void cmd_unregister_keyword(char const *keyword);
 void cmd_end(void);
 
 #endif
