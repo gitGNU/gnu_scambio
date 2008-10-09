cc798aa4fd80f6891eba0025d5890e3b352a8495
Cedric Cellier <rixed@deb.happyleptic.org>
2008-10-09 10:44:48 +0200

    MereCal : relation between events and calendar days materialized

--- merecal/cal_view.c
+++ merecal/cal_view.c
@@ -19,11 +19,32 @@ enum {
 
 static GtkWidget *calendar;
 
+struct day2event {
+	TAILQ_ENTRY(day2event) entry;
+	struct cal_event *event;
+};
+
+static TAILQ_HEAD(day2events, day2event) day2events[31];
+
 /*
  * Content
  */
 
-static void add_event(struct cal_event *ce)
+static void day2event_new(struct cal_event *ce, unsigned day)
+{
+	struct day2event *d2e = malloc(sizeof(*d2e));
+	if (! d2e) with_error(ENOMEM, "malloc d2e") return;
+	d2e->event = ce;
+	TAILQ_INSERT_TAIL(day2events+day, d2e, entry);
+}
+
+static void day2event_del(struct day2event *d2e, unsigned day)
+{
+	TAILQ_REMOVE(day2events+day, d2e, entry);
+	free(d2e);
+}
+
+static void display_event(struct cal_event *ce)
 {
 	(void)ce;	// TODO
 }
@@ -37,31 +58,44 @@ static void reset_day(void)
 	struct cal_date day_start, day_stop;
 	cal_date_ctor(&day_start, year, month, 1, 0, 0);
 	cal_date_ctor(&day_stop,  year, month, 31, 23, 29);
-	foreach_event_between(&day_start, &day_stop, add_event);
+	struct day2event *d2e;
+	TAILQ_FOREACH(d2e, day2events+day, entry) {
+		display_event(d2e->event);
+	}
 	cal_date_dtor(&day_start);
 	cal_date_dtor(&day_stop);
 }
 
-static void mark_days(struct cal_event *ce)
+static void select_event(struct cal_event *ce)
 {
 	struct cal_date *end = cal_date_is_set(&ce->stop) ? &ce->stop : &ce->start;
-	struct cal_date period = ce->start;
-	while (cal_date_compare(&period, end) <= 0 && period.day <= 31) {
-		gtk_calendar_mark_day(GTK_CALENDAR(calendar), period.day ++);
+	for (
+		struct cal_date period = ce->start;
+		cal_date_compare(&period, end) <= 0 && period.day <= 31;
+		period.day ++
+	) {
+		gtk_calendar_mark_day(GTK_CALENDAR(calendar), period.day);
+		day2event_new(ce, period.day);
 	}
 }
 
-// month changed (or the calendar was just created) : mark the correct days
-// and display the events for the new selected day
+// month changed (or the calendar was just created) : prepare the
+// day2events array, and mark the used days
 static void reset_month(void)
 {
+	// Clean per-month datas
 	gtk_calendar_clear_marks(GTK_CALENDAR(calendar));
+	for (unsigned i=0; i<sizeof_array(day2events); i++) {
+		struct day2event *d2e;
+		while (NULL != (d2e = TAILQ_FIRST(day2events+i))) day2event_del(d2e, i);
+	}
+	// Rebuild
 	guint year, month, day;
 	gtk_calendar_get_date(GTK_CALENDAR(calendar), &year, &month, &day);
 	struct cal_date month_start, month_stop;
 	cal_date_ctor(&month_start, year, month, 1, 0, 0);
 	cal_date_ctor(&month_stop,  year, month, 31, 23, 29);
-	foreach_event_between(&month_start, &month_stop, mark_days);
+	foreach_event_between(&month_start, &month_stop, select_event);
 	cal_date_dtor(&month_start);
 	cal_date_dtor(&month_stop);
 	reset_day();
@@ -94,6 +128,10 @@ gchar *cal_details(GtkCalendar *calendar, guint year, guint month, guint day, gp
 
 GtkWidget *make_cal_window(void)
 {
+	for (unsigned i=0; i<sizeof_array(day2events); i++) {
+		TAILQ_INIT(day2events+i);
+	}
+
 	GtkWidget *window = make_window(NULL);
 	GtkWidget *vbox = gtk_vbox_new(FALSE, 1);
 	
