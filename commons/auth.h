95163d0a7566739942f85d4f3b5d2b319c53220d
cedric cellier <rixed@free.fr>
2008-10-11 22:13:20 +0200

    licence and ideas

--- commons/auth.h
+++ commons/auth.h
@@ -15,15 +15,20 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with Scambio.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef MEREMAIL_H_081007
-#define MEREMAIL_H_081007
-
-#include "merelib.h"
-#include "scambio.h"
-#include "scambio/mdir.h"
-#include "scambio/header.h"
-
-GtkWidget *make_list_window(char const *folder);
-GtkWidget *make_folder_window(char const *parent);
+#ifndef AUTH_H_081011
+#define AUTH_H_081011
+/* The server needs to authenticate each user and the communication needs to be
+ * crypted - communication which can be UDP.
+ * Pre-shared key symetric cyphering is all we need.
+ * Secrets are stored in a directory containing a file per key, named with the user
+ * whom its the key (on the server as well as the client), so that it's easy to add
+ * accounts.
+ *
+ * This common routines read this directory for the key, encrypt and decrypt
+ * messages, and so on.
+ * It would be a good idea to compress the flow _before_ encrypting.
+ * Also, the same keys should serve to discuss with the file server(s), which then
+ * must also know all users keys.
+ */
 
 #endif
